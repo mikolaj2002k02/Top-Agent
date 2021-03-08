76be1384e951f899a5a3d1349a51a4ac7e9d960b
mikolaj2002k02 <mikolaj2002k02@gmail.com>
2021-03-08 08:56:14 +0100

    without view

---
+++ script.js
@@ -0,0 +1,7 @@
+import Glide from '@glidejs/glide'
+
+new Glide('.glide').mount()
+
+import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
+
+new Glide('.glide').mount({ Controls, Breakpoints })
